2585402e9c9ef3d70308ce6704544ca5f05abc1a
Dylan Govender <134143760+dylan-govend@users.noreply.github.com>
2024-02-02 10:03:41 +0200

    HackerRank - Power - Mod Power

---
+++ Power - Mod Power/ModPower.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+if __name__ == '__main__':
+    a = int(input().strip())
+    b = int(input().strip())
+    m = int(input().strip())
+    
+    print(str(pow(a, b)))
+    print(str(pow(a, b, m)))
+
+#LINK: https://www.hackerrank.com/challenges/python-power-mod-power/submissions/code/367727582
