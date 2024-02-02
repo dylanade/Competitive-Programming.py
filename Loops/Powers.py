f96f99c6bb9abc9c5694b7d4d7a5b1ad3b1f279e
Dylan Govender <134143760+dylan-govend@users.noreply.github.com>
2024-02-02 10:01:52 +0200

    HackerRank - Loops

---
+++ Loops/Powers.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(input())
+    for num in range(n):
+        print(num ** 2)
+
+#LINK: https://www.hackerrank.com/challenges/python-loops/submissions/code/367726701
