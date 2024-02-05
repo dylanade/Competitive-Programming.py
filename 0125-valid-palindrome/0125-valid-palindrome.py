645d23d54341408549cab6246f45bf9dc1c83f7e
Dylan Govender <134143760+dylan-govend@users.noreply.github.com>
2024-02-05 16:42:48 +0200

    Time: 39 ms (89.71%), Space: 17.1 MB (78.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,7 +4,7 @@ class Solution:
         
         clean_s = ""
         for char in s:
-            if char.isalpha() or char in "0123456789":
+            if char.isalpha() or char.isdigit():
                 clean_s += char
                 
         s = clean_s.lower()
