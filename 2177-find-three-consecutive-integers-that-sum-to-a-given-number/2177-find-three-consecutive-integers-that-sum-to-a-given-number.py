df4799c1393e755d2c4d11bb34de8e1fbbaaa7aa
Dylan Govender <134143760+dylan-govend@users.noreply.github.com>
2024-02-06 22:23:54 +0200

    Time: 38 ms (48.39%), Space: 16.7 MB (51.61%) - LeetHub

--- 2177-find-three-consecutive-integers-that-sum-to-a-given-number/2177-find-three-consecutive-integers-that-sum-to-a-given-number.py
+++ 2177-find-three-consecutive-integers-that-sum-to-a-given-number/2177-find-three-consecutive-integers-that-sum-to-a-given-number.py
@@ -6,8 +6,8 @@ class Solution:
             we are looking for (x) + (x+1) + (x+2) == num
             3x + 3 = num
             3x = num-3
-            x = (num-3)/3
-            if x is not a float, return [x, x+1, x+2] otherwise return []
+            x = (num-3) / 3
+            if x is not a decimal, return [x, x+1, x+2] otherwise return []
         '''
         
         x = (num-3) / 3
