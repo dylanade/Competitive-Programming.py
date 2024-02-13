e2f949ea0a4056cc045f8ac10eb4cbbfa13b93ec
Dylan Govender <134143760+dylan-govend@users.noreply.github.com>
2024-02-13 20:53:24 +0200

    Time: 64 ms (56.75%), Space: 17.8 MB (57.92%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mapping = {}
+        
+        for i in range(len(nums)):
+            if (int(target - nums[i]) in mapping.keys()):
+                return [mapping.get(target-nums[i]), i]
+            mapping[nums[i]] = i
+            
+        return []
+        
