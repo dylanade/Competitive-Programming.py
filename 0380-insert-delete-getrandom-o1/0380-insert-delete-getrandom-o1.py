509ed99dbba174349322b6a3b135c9058979d1f7
Dylan Govender <134143760+dylan-govend@users.noreply.github.com>
2024-02-16 20:31:04 +0200

    Time: 410 ms (36.76%), Space: 64.1 MB (79.38%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -8,11 +8,13 @@ class RandomizedSet:
         if val not in self.random_dict.keys():
             self.random_dict[val] = 0
             return True
+        return False
 
     def remove(self, val: int) -> bool:
         if val in self.random_dict.keys():
             del self.random_dict[val]
             return True
+        return False
         
 
     def getRandom(self) -> int: 
