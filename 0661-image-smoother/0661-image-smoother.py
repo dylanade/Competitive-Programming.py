42c6bdc9fccf530e1080a8237cf5c517ce7b7886
Dylan Govender <134143760+dylanguish@users.noreply.github.com>
2024-02-22 21:08:58 +0200

    Time: 286 ms (97.61%), Space: 17.2 MB (86.98%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.py
@@ -0,0 +1,49 @@
+class Solution:
+    def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+        
+        m, n = len(img), len(img[0])
+        ans = [[0] * n for _ in range(m)]
+        
+        for i in range(m):
+            for j in range(n):
+                curr_sum, count = img[i][j], 1
+                
+                if j != 0:
+                    curr_sum += img[i][j-1]
+                    count += 1
+                    
+                if j != n-1:
+                    curr_sum += img[i][j+1]
+                    count += 1
+                    
+                if i != 0:
+                    curr_sum += img[i-1][j]
+                    count += 1
+                    
+                    if j != 0:
+                        curr_sum += img[i-1][j-1]
+                        count += 1
+                        
+                    if j != n-1:
+                        curr_sum += img[i-1][j+1]
+                        count += 1
+                    
+                if i != m-1:
+                    curr_sum += img[i+1][j]
+                    count += 1
+                    
+                    if j != 0:
+                        curr_sum += img[i+1][j-1]
+                        count += 1
+                        
+                    if j != n-1:
+                        curr_sum += img[i+1][j+1]
+                        count += 1
+                        
+                    
+                avg = curr_sum // count
+                curr_sum = avg
+                ans[i][j] = avg
+                
+        return ans
+                
