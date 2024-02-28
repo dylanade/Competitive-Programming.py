bcc9d3fb8c455aff3b7c383d03d24709ca5cad5d
Dylan Govender <134143760+dylanguish@users.noreply.github.com>
2024-02-28 09:22:38 +0200

    Time: 100 ms (54.82%), Space: 18 MB (32.30%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,14 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        occur_map = {}
+        freq_mp = {}
         answer = []
         constraint = int(len(nums)/3) #n = len(nums) check: occurence > [n/3]
         
         for num in nums:
-            occur_map[num] = occur_map.get(num, 0) + 1
+            freq_mp[num] = freq_mp.get(num, 0) + 1
             
-        for key in occur_map:
-            if (occur_map.get(key) > constraint):
+        for key in freq_mp:
+            if freq_mp.get(key) > constraint:
                 answer.append(key)
                 
         return answer
