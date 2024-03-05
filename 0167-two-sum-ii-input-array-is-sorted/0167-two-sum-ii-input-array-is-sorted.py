507b2dc10d056bb2589985856bbc37d0cefb92c8
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-03-05 21:58:26 +0200

    Time: 111 ms (28.00%), Space: 17.4 MB (68.07%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,9 +1,14 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        #Linear O(N), Space O(N)
         mp = {}
         for i, num in enumerate(numbers):
             s = target - num
             if s in mp:
                 return [mp[s]+1, i+1]
             mp[num] = i
+        
+
+        
+        
         
