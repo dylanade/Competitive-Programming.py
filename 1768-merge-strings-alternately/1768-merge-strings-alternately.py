3e0647f932d057cd6ef6db8f1962ebccba40953e
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-03-08 14:42:01 +0200

    Time: 31 ms (82.43%), Space: 16.8 MB (22.04%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,36 +1,22 @@
-class Solution(object):
-    def mergeAlternately(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: str
-        """
-        tail = []
-        merged = []
-        word1Chars = list(word1)
-        word2Chars = list(word2)
-        
-        #get the length of each words
-        len_1 = len(word1Chars)
-        len_2 = len(word2Chars)
-        
-        #merge the list
-        minLen = min(len_1, len_2)
-        for i in range(minLen*2):
-            if (i % 2 == 0):
-                merged.append(word1Chars[i/2])
-            else:
-                merged.append(word2Chars[(i-1)/2])
-                
-        #get the tail of the list
-        if (len_1 > len_2):
-            tail = word1Chars[len_2:]
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        merged, tail = [], []
+        char1, char2 = list(word1), list(word2)
+        p, q = len(word1), len(word2)
+
+        if p > q:
+            tail = char1[q:]
         else:
-            tail = word2Chars[len_1:]
-            
-        #combine the tail and the merged list
-        merged = merged + tail
-
-        return "".join(merged)
-
-
+            tail = char2[p:]
+        
+        for i in range(min(p,q)*2):
+            if i % 2 == 0:
+                merged.append(char1[i//2])
+            else:
+                merged.append(char2[(i-1)//2])
+        
+        return "".join(merged + tail)
+        
+        
+        
+        
