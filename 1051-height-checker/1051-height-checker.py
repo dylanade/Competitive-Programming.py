7f8721c203d65dd630a0a89b2b87d86b775b4ef5
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-03-14 17:18:41 +0200

    Time: 45 ms (22.08%), Space: 16.6 MB (42.71%) - LeetHub

--- 1051-height-checker/1051-height-checker.py
+++ 1051-height-checker/1051-height-checker.py
@@ -1,13 +1,21 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        expected = sorted(heights)
-        count = 0
-        
+        count = [0] * (max(heights) + 1)
+        res = 0
+
         for i in range(len(heights)):
-            if heights[i] != expected[i]:
-                count += 1
+            count[heights[i]] += 1
+
+        for i in range(1, len(count)):
+            count[i] += count[i-1]
+
+        for i in range(len(heights)):
+            if count[heights[i] - 1] <= i < count[heights[i]]:
+                continue
+            else:
+                res += 1
                 
-        return count
+        return res
                 
         
         
