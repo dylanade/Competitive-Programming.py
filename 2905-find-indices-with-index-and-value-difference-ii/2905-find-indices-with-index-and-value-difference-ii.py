f78eacec92757e0ef25e40de3de95f4d9058a218
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-03-14 19:52:39 +0200

    Time: 521 ms (95.43%), Space: 32 MB (72.00%) - LeetHub

---
+++ 2905-find-indices-with-index-and-value-difference-ii/2905-find-indices-with-index-and-value-difference-ii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findIndices(self, nums: List[int], iD: int, vD: int) -> List[int]:
+        if iD >= len(nums):
+            return [-1, -1]
+        
+        minV = float('inf')
+        maxV = float('-inf')
+        minI = 0
+        maxI = 0
+        i = len(nums) - iD - 1
+        j = len(nums) - 1
+        
+        while i >= 0:
+            if nums[j] < minV:
+                minV = nums[j]
+                minI = j
+                
+            if nums[j] > maxV:
+                maxV = nums[j]
+                maxI = j
+            
+            if abs(nums[i] - minV) >= vD:
+                return [i, minI]
+            
+            if abs(nums[i] - maxV) >= vD:
+                return [i, maxI]
+            
+            i -= 1
+            j -= 1
+        
+        return [-1, -1]
+                    
+    
