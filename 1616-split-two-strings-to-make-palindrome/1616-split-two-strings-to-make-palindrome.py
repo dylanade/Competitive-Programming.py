554b27b65d59af7a422950ee9a9f5dea94499a07
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-03-21 19:24:09 +0200

    Time: 74 ms (55.74%), Space: 17.9 MB (62.30%) - LeetHub

--- 1616-split-two-strings-to-make-palindrome/1616-split-two-strings-to-make-palindrome.py
+++ 1616-split-two-strings-to-make-palindrome/1616-split-two-strings-to-make-palindrome.py
@@ -1,24 +1,19 @@
 class Solution:
     def checkPalindromeFormation(self, a: str, b: str) -> bool:
-        
         for x, y in [[a, b], [b, a]]:
             i, j = 0, len(x)-1
             
-            while x[i] == y[j] and i<len(x) and j>0:
+            while x[i]==y[j] and i<len(x) and j>0:
                 i += 1
                 j -= 1
                 
             midx = x[i:j+1]
             midy = y[i:j+1]
             
-            if (midx == midx[::-1] or midy == midy[::-1]):
+            if (midx==midx[::-1] or midy==midy[::-1]):
                 return True
             
-        return False 
-                
-        
-        
-        
+        return False    
         
         #prefixes of a
         #u
@@ -34,5 +29,4 @@ class Solution:
         #alu
         #zalu
         #izalu
-        
         
