87628f5747974a28a40b0c3463afc54e0b30b992
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-03-23 17:30:10 +0200

    Time: 818 ms (8.17%), Space: 18.1 MB (31.67%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,24 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-
-        # for num in nums:
-        #     if num == 0:
-        #         nums.append(num)
-        #         nums.remove(num)
         
-        #Using Two-Pointers Seeker and Placeholder
-        
-        holder = 0
-        seeker = 0
-        
-        while seeker < len(nums):
-            if nums[seeker] != 0:
-                nums[seeker], nums[holder] = nums[holder], nums[seeker]
-                holder += 1
-            seeker += 1
-            
-        
-                
-        
-        
+        for num in nums:
+            if num == 0:
+                nums.append(0)
+                nums.remove(num)
