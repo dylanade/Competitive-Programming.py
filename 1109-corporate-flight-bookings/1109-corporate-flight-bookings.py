6dd4b71ac16fe9bb9c965580d3315ffd8009eead
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-04-08 16:23:30 +0200

    Time: 683 ms (20.65%), Space: 30.8 MB (35.33%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
@@ -6,8 +6,17 @@ class Solution:
             prefix[first-1] += seats
             prefix[last] -= seats
             
-        cumulate = 0
         for i in range(1, n):
             prefix[i] += prefix[i-1]
             
         return prefix[:n]
+    
+        res = [0] * n
+
+#         for start, end, seats in bookings:
+#             start_ = start - 1
+#             end_ = end - 1
+#             res[start_] += seats
+#             if end < n:
+#                 res[end] -= seats  
+#         return accumulate(res)
