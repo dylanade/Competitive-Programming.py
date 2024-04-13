92b6e94ad6cfbf214559b44ce534cac97130ad2f
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-04-13 19:14:45 +0200

    Time: 35 ms (65.55%), Space: 16.7 MB (23.35%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
@@ -3,7 +3,7 @@ class Solution:
         
         count_factor = 0
         for i in range(1, n+1):
-            if n%i == 0:
+            if not n%i:
                 count_factor += 1
                 if count_factor == k:
                     return i
