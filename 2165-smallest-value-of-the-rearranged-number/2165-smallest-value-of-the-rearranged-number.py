5bf2f1a6ef04b3ff6131ab86cd2b5e3f77fab2ae
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-04-14 17:46:36 +0200

    Time: 26 ms (97.91%), Space: 16.5 MB (93.98%) - LeetHub

--- 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.py
+++ 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.py
@@ -1,20 +1,21 @@
 class Solution:
     def smallestNumber(self, num: int) -> int:
-        ans = ''
-        i = 0
-        if num >= -9 and num <= 9:
-            return num
-        elif num > 9:
-            result = sorted(list(str(num)))
-            while result[i] == '0' and i < len(result):
+        if num > 0:
+            s = sorted(str(num))
+            min_chr = min(s)
+            n = len(s)
+            i = 0
+            while i < n:
+                if s[i] != '0':
+                    min_chr = s[i]
+                    s[0], s[i] = s[i], s[0]
+                    break
                 i += 1
-            last = ''.join(result[i+1:])
-            ans += f"{result[i]}{i*'0'}{last}"
+            return int(''.join(s))
         else:
-            n = str(num)[1:]
-            result = sorted(list(str(num)), reverse=True)
-            while result[i] == '0' and i < len(result):
-                i += 1
-            last = ''.join(result[i+1:len(result)-1])
-            ans += f"-{result[i]}{i*'0'}{last}"
-        return int(ans)
+            s = sorted(str(abs(num)), reverse=True)
+            return int('-'+''.join(s))
+        
+        
+        
+        
