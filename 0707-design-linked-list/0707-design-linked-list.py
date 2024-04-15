4f3758c015095d07ae5d32e336e256861ab1a085
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-04-15 21:57:22 +0200

    Time: 117 ms (50.05%), Space: 17.5 MB (48.71%) - LeetHub

---
+++ 0707-design-linked-list/0707-design-linked-list.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, value=0, next=None):
+        self.value = value
+        self.next = next
+        
+class MyLinkedList:  
+    def __init__(self):
+        self.head = Node(0)
+        self.size = 0
+        
+    def get(self, index: int) -> int:
+        if index < 0 or index >= self.size: return -1
+        current = self.head
+        for _ in range(index+1): current = current.next
+        return current.value
+
+    def addAtHead(self, val: int) -> None:
+        self.addAtIndex(0, val)
+        
+    def addAtTail(self, val: int) -> None:
+        self.addAtIndex(self.size, val)
+        
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index < 0 or index > self.size: return
+        current = self.head
+        for _ in range(index): current = current.next
+        new_node = Node(val, current.next)
+        current.next = new_node
+        self.size += 1
+
+    def deleteAtIndex(self, index: int) -> None:
+        if index < 0 or index >= self.size: return
+        current = self.head
+        for _ in range(index): current = current.next
+        current.next = current.next.next
+        self.size -= 1  
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
