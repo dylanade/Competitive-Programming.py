79fad959c851a5c6c5557a82f035f6e3c0131c27
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-04-16 20:05:55 +0200

    Time: 128 ms (28.68%), Space: 17.4 MB (48.71%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.py
+++ 0707-design-linked-list/0707-design-linked-list.py
@@ -9,32 +9,57 @@ class MyLinkedList:
         self.size = 0
         
     def get(self, index: int) -> int:
-        if index < 0 or index >= self.size: return -1
+        if index < 0 or index >= self.size: 
+            return -1
         current = self.head
-        for _ in range(index+1): current = current.next
+        for _ in range(index+1): 
+            current = current.next
         return current.value
 
     def addAtHead(self, val: int) -> None:
+        # new_node = Node(val, self.head)
+        # self.head = new_node
+        # self.size += 1
         self.addAtIndex(0, val)
+        #self.printList()
         
     def addAtTail(self, val: int) -> None:
+        # current = self.head
+        # for _ in range(self.size):
+        #     current = current.next
+        # current.next = Node(val, None)
+        # self.size += 1
         self.addAtIndex(self.size, val)
+        #self.printList()
         
     def addAtIndex(self, index: int, val: int) -> None:
-        if index < 0 or index > self.size: return
+        if index < 0 or index > self.size: 
+            return
         current = self.head
-        for _ in range(index): current = current.next
+        for _ in range(index): 
+            current = current.next
         new_node = Node(val, current.next)
         current.next = new_node
         self.size += 1
+        #self.printList()
 
     def deleteAtIndex(self, index: int) -> None:
-        if index < 0 or index >= self.size: return
+        if index < 0 or index >= self.size: 
+            return
         current = self.head
-        for _ in range(index): current = current.next
+        for _ in range(index): 
+            current = current.next
         current.next = (current.next).next
         self.size -= 1  
-
+        #self.printList()
+        
+    def printList(self):
+        array = [0]*self.size
+        current = self.head
+        for i in range(self.size):
+            array[i] = current.value
+            current = current.next
+        print(array)
 
 # Your MyLinkedList object will be instantiated and called as such:
 # obj = MyLinkedList()
