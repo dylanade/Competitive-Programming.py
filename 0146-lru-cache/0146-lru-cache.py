255bc2322b25f7a7d2a6b4ba7d4148e474e6aed9
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-04-22 20:24:59 +0200

    Time: 1008 ms (9.54%), Space: 77 MB (98.14%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,25 +1,24 @@
 class LRUCache:
-
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = OrderedDict()
+        self.cache = {}
+        self.stack = []
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            self.cache.move_to_end(key)
-            return self.cache[key]
-        else:
-            return -1
+            LRU_value = self.cache[key]
+            del self.cache[key]
+            self.cache[key] = LRU_value
+        return self.cache.get(key, -1)
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
+            del self.cache[key]
             self.cache[key] = value
-            self.cache.move_to_end(key)
         else:
+            if len(self.cache) >= self.capacity:
+                del self.cache[list(self.cache.keys())[0]]
             self.cache[key] = value
-            if len(self.cache) > self.capacity: 
-                self.cache.popitem(0)
-
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
