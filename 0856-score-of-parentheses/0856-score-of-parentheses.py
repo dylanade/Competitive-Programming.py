3cfbdaae251bac6523c5b1b31f51e056d912d1a2
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-04-23 21:44:18 +0200

    Time: 35 ms (52.92%), Space: 16.5 MB (88.18%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.py
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -1,38 +1,36 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
-#         stack = []
-#         score = 0
-        
-#         for c in s:
-#             if c=='(':
-#                 stack.append(score)
-#                 score = 0
-#             elif c==')':
-#                 score = 1 if score==0 else score*2
-#                 score = score + stack.pop()
-#             else:
-#                 score = score + int(char)
-            
-#         return score
-    
         stack = []
         score = 0
-        i = 0
-    
-        while i < len(s):
-            if i+1<len(s) and s[i]=='(' and s[i+1]==')':
-                score += 1
-                i += 2
-            elif s[i]=='(':
+        
+        for c in s:
+            if c=='(':
                 stack.append(score)
                 score = 0
-                i += 1
-            elif s[i]==')':
-                if stack:
-                    score = stack.pop() + 2*score
-                i += 1
-
+            elif c==')':
+                score = 1 if score==0 else score*2
+                score = score + stack.pop()
+            
         return score
+    
+#         stack = []
+#         score = 0
+#         i = 0
+    
+#         while i < len(s):
+#             if i+1<len(s) and s[i]=='(' and s[i+1]==')':
+#                 score += 1
+#                 i += 2
+#             elif s[i]=='(':
+#                 stack.append(score)
+#                 score = 0
+#                 i += 1
+#             elif s[i]==')':
+#                 if stack:
+#                     score = stack.pop() + 2*score
+#                 i += 1
+
+#         return score
 
             
         
