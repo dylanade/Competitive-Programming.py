c741715b9bda881d0ec82c7c7057f251de3530a7
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-04-23 21:44:39 +0200

    Time: 27 ms (92.12%), Space: 16.6 MB (38.00%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.py
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -7,32 +7,8 @@ class Solution:
             if c=='(':
                 stack.append(score)
                 score = 0
-            elif c==')':
+            else:
                 score = 1 if score==0 else score*2
                 score = score + stack.pop()
             
         return score
-    
-#         stack = []
-#         score = 0
-#         i = 0
-    
-#         while i < len(s):
-#             if i+1<len(s) and s[i]=='(' and s[i+1]==')':
-#                 score += 1
-#                 i += 2
-#             elif s[i]=='(':
-#                 stack.append(score)
-#                 score = 0
-#                 i += 1
-#             elif s[i]==')':
-#                 if stack:
-#                     score = stack.pop() + 2*score
-#                 i += 1
-
-#         return score
-
-            
-        
-        
-            
