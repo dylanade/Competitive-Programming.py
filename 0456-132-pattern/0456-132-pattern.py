dff9c16cfd7f5b3b2334e84714ca843e952b13c0
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-04-26 19:01:39 +0200

    Time: 463 ms (99.55%), Space: 37.8 MB (20.77%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -1,13 +1,19 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
         stack = []
-        last = float(-10**11)
-        
+        k = float('-inf')
+
+        # Iterate through the array in reverse order
         for num in reversed(nums):
-            if num < last:
+            # If we find a number smaller than k, 
+            # it means we've found a valid pattern
+            if num < k:
                 return True
+            # Keep updating k and popping elements from the stack
             while stack and stack[-1] < num:
-                last = stack.pop()
+                k = stack.pop()
+            # Push the current number onto the stack
             stack.append(num)
-            
+
+        # If no valid pattern is found, return False
         return False
