962a64b099034c4e2835a0a1251dc212d418c2a4
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-04-26 19:52:20 +0200

    Time: 471 ms (97.54%), Space: 37.8 MB (20.77%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -3,17 +3,11 @@ class Solution:
         stack = []
         k = float('-inf')
 
-        # Iterate through the array in reverse order
         for num in reversed(nums):
-            # If we find a number smaller than k, 
-            # it means we've found a valid pattern
             if num < k:
                 return True
-            # Keep updating k and popping elements from the stack
             while stack and stack[-1] < num:
                 k = stack.pop()
-            # Push the current number onto the stack
             stack.append(num)
 
-        # If no valid pattern is found, return False
         return False
