97f4116c832d139b52f7e5456b9b633a86730ef4
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-05-01 20:18:12 +0200

    Time: 536 ms (30.38%), Space: 21.1 MB (37.97%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -5,11 +5,12 @@ class Solution:
             inverted += "0" if c == "1" else "1"
         return inverted
     
-    def findS(self, n: int) -> str:
+    def findBinaryString(self, n: int) -> str:
         if n == 1:
             return "0"
         else:
-            return self.findS(n-1) + "1" + self.invert(self.findS(n-1))[::-1]
+            result = self.findBinaryString(n-1)
+            return result + "1" + self.invert(result)[::-1]
     
     def findKthBit(self, n: int, k: int) -> str:
-        return self.findS(n)[k-1]
+        return self.findBinaryString(n)[k-1]
