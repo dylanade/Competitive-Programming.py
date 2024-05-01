9ef7d9035923da910fa578f92daeff7c9d9394c3
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-05-01 21:03:20 +0200

    Time: 1183 ms (34.31%), Space: 16.6 MB (38.13%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,10 +1,5 @@
 class Solution:
     def predictTheWinner(self, nums: list[int]) -> bool:
-        @cache
         def score(i: int, j: int) -> int:
-            return (i <= j) and max(
-                nums[i] - score(i + 1, j),
-                nums[j] - score(i, j - 1),
-            )
-        
-        return len(nums) % 2 == 0 or score(0, len(nums) - 1) >= 0
+            return (i<=j) and max(nums[i]-score(i+1, j), nums[j]-score(i, j-1))
+        return len(nums)%2 == 0 or score(0, len(nums)-1) >= 0
