fd6ca600ba9862a7fd067212e5dc9ef9b6f9dac3
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-05-14 22:29:30 +0200

    Time: 2517 ms (7.93%), Space: 22.5 MB (73.17%) - LeetHub

--- 0911-online-election/0911-online-election.py
+++ 0911-online-election/0911-online-election.py
@@ -1,11 +1,10 @@
 class TopVotedCandidate:
-    
     def __init__(self, persons: List[int], times: List[int]):
         self.persons = persons
         self.times = times
         
         n = len(persons)
-        self.leading_votes = n * [0]
+        self.leading_votes = n*[0]
         count_votes = defaultdict(int)
         top_candidate = persons[0]
         max_vote = 1
@@ -16,13 +15,10 @@ class TopVotedCandidate:
                 max_vote = count_votes[persons[i]]
                 top_candidate = persons[i]
             self.leading_votes[i] = top_candidate
+        #print(self.leading_votes)
             
     def q(self, t: int) -> int:
         l, r = 0, len(self.times)-1
-        
-        if t in self.times:
-            return self.leading_votes[self.times.index(t)]
-        
         relative_t = 0
         while l <= r:
             mid = (l+r)//2
@@ -34,7 +30,6 @@ class TopVotedCandidate:
                 
         return self.leading_votes[self.times.index(relative_t)]
             
-    
 # Your TopVotedCandidate object will be instantiated and called as such:
 # obj = TopVotedCandidate(persons, times)
 # param_1 = obj.q(t)
