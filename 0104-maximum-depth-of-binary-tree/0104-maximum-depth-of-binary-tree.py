b8378ca16447204ee345271aaa4cba2bdd05dd1e
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-05-16 19:57:37 +0200

    Time: 43 ms (35.99%), Space: 17.6 MB (50.62%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -6,19 +6,29 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        def traverse(height, node):
-                if node:
-                    if node.left and node.right:
-                        height_left = traverse(height, node.left)
-                        height_right = traverse(height, node.right)
-                        height += max(height_left, height_right)
-                    elif node.left:
-                        height += traverse(height, node.left)
-                    elif node.right:
-                        height += traverse(height, node.right)
-                return height
-        if root:
-            return traverse(1, root)
-        else:
-            return 0
+        def traverse(node):
+            if not node:
+                return 0
+            else:
+                left = 1 + traverse(node.left)
+                right = 1 + traverse(node.right)
+                return max(left, right)
+        return traverse(root)
+
+        # def traverse(height, node):
+        #         if node:
+        #             if node.left and node.right:
+        #                 height_left = traverse(height, node.left)
+        #                 height_right = traverse(height, node.right)
+        #                 height += max(height_left, height_right)
+        #             elif node.left:
+        #                 height += traverse(height, node.left)
+        #             elif node.right:
+        #                 height += traverse(height, node.right)
+        #         return height
+                
+        # if root:
+        #     return traverse(1, root)
+        # else:
+        #     return 0
         
