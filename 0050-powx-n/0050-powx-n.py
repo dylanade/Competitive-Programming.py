bcc8eb2abb7333a6bdd6f718052e35fb1695b178
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-05-29 18:05:30 +0200

    Time: 41 ms (16.36%), Space: 16.5 MB (95.56%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,4 +1,27 @@
 class Solution:
-    def myPow(self, x: float, n: int) -> float:
-        return x ** n
+    def myPow(self, x: float, n:int) -> float:
+        # TLE: 291/307 TC
+        # is_negative = n < 0
+        # n = -n if is_negative else n
+
+        # answer = 1
+        # for _ in range(n):
+        #     answer *= x
+        # return 1/answer if is_negative else answer
+
+        # DIVIDE and CONQUER 2^10 -> 2^5 * 2^5 = 2^10
+        # cutting the work in half each time: O(log2(n))
+
+        def helper(x, n):
+            if x == 0: return 0 # any number powered to 0 is 0
+            if n == 0: return 1 
+        
+            answer = helper(x, n//2)
+            answer = answer * answer
+            return x * answer if n % 2 else answer
+
+        answer = helper(x, abs(n))
+        return 1/answer if n < 0 else answer
+
+
         
