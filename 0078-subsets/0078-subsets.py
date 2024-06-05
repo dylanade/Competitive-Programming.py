25300650b6970ee70e06908fac31a16ab8d5f08f
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-06-05 21:58:19 +0200

    Time: 33 ms (80.9%), Space: 16.6 MB (75.18%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,29 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # 2^n = number of subsets
+        # Worst Case: O(n * 2^n)
+
+        answer = []
+        subset = []
+        
+        def dfs(i): # backtracking using dfs
+            # base cases
+            if i >= len(nums):
+                answer.append(subset[:])
+                return
+
+            # decisions
+            # decision to include nums[i]
+            subset.append(nums[i])
+            dfs(i + 1)
+
+            # decision NOT to include nums[i]
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return answer
+
+            
+
+
