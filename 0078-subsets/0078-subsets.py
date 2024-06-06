affc9cc37ea8baf1b06e64ee904650a86ba7d247
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-06-06 20:10:18 +0200

    Time: 45 ms (8.35%), Space: 16.6 MB (96.7%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,29 +1,33 @@
+from itertools import chain, combinations
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # 2^n = number of subsets
-        # Worst Case: O(n * 2^n)
+        def powerset(iterable):
+            s = list(iterable)
+            return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
-        answer = []
-        subset = []
-        
-        def dfs(i): # backtracking using dfs
-            # base cases
-            if i >= len(nums):
-                answer.append(subset[:])
-                return
+        # # 2^n = number of subsets
+        # # Worst Case: O(n * 2^n)
 
-            # decisions
-            # decision to include nums[i]
-            subset.append(nums[i])
-            dfs(i + 1)
+        # answer = []
+        # subset = []
 
-            # decision NOT to include nums[i]
-            subset.pop()
-            dfs(i + 1)
+        # def dfs(i): # backtracking using dfs
+        #     # base cases
+        #     if i >= len(nums):
+        #         answer.append(subset[:])
+        #         return
 
-        dfs(0)
-        return answer
+        #     # decisions
+        #     # decision to include nums[i]
+        #     subset.append(nums[i])
+        #     dfs(i + 1)
 
-            
+        #     # decision NOT to include nums[i]
+        #     subset.pop()
+        #     dfs(i + 1)
 
+        # dfs(0)
+        # return answer
 
+        return powerset(nums)
