769f63409275e8e40030b4a7b7a771a49df95069
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-06-06 19:46:58 +0200

    Time: 6405 ms (9.45%), Space: 16.1 MB (100%) - LeetHub

--- 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.py
+++ 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.py
@@ -2,17 +2,19 @@ class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
         unfairness = float("inf") # minimum unfairness
         distribution = [0] * k # distribution between k children
+        # Time: O(k^n) -> Braches ^ Depth
+        # Space: O(n + k)
+        # n is independent of k
 
         def backtrack(i): # init to start from first child
             nonlocal unfairness
-            max_cookies = max(distribution)
 
             # base cases
             if i == len(cookies):
-                unfairness = min(unfairness, max_cookies)
+                unfairness = min(unfairness, max(distribution))
                 return
             
-            if unfairness <= max_cookies:
+            if unfairness <= max(distribution):
                 return
 
             # decisions
