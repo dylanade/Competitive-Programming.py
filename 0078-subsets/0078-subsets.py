343bb314b669eb144b55f633c0d710c2bf13e125
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-06-06 20:16:03 +0200

    Time: 42 ms (21.39%), Space: 16.6 MB (75.18%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,12 +2,28 @@ from itertools import chain, combinations
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def powerset(iterable):
-            s = list(iterable)
-            return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+        # def powerset(iterable):
+        #     s = list(iterable)
+        #     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+        # return powerset(nums)
 
-        # # 2^n = number of subsets
-        # # Worst Case: O(n * 2^n)
+        def get_powerset(nums):
+            if len(nums) == 0:
+                return [[]]
+
+            powerset = []
+            first_element = nums[0]
+            remaining_list = nums[1:]
+
+            for partial_subset in get_powerset(remaining_list):
+                powerset.append(partial_subset)
+                powerset.append(partial_subset[:] + [first_element])
+
+            return powerset
+        return get_powerset(nums)
+
+        # 2^n = number of subsets
+        # Worst Case: O(n * 2^n)
 
         # answer = []
         # subset = []
@@ -29,5 +45,3 @@ class Solution:
 
         # dfs(0)
         # return answer
-
-        return powerset(nums)
