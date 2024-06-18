1cb38e271a92024ba9c815c5b07962aea7209a3f
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-06-18 20:55:29 +0200

    Time: 366 ms (58.04%), Space: 16.9 MB (55.46%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -30,9 +30,7 @@ class Solution:
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
                     perimeter += 4
-                    if i > 0 and grid[i - 1][j] == 1:
-                        perimeter -= 2
-                    if j > 0 and grid[i][j - 1] == 1:
-                        perimeter -= 2
+                    if i > 0 and grid[i - 1][j] == 1: perimeter -= 2
+                    if j > 0 and grid[i][j - 1] == 1: perimeter -= 2
 
         return perimeter
