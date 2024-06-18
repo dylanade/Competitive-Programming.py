47be6f85387dc8aca48fc060128566ac9c7d5155
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-06-18 20:46:01 +0200

    Time: 493 ms (8.73%), Space: 23.8 MB (8.32%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -0,0 +1,25 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        # O(n*m) # TC
+        # O(n*m) # SC
+        visit = set() # cells visited
+
+        def dfs(i, j):
+            if (i >= len(grid) or j >= len(grid[0]) 
+                or i < 0 or j < 0 or grid[i][j] == 0):
+                return 1
+
+            if (i, j) in visit:
+                return 0
+
+            visit.add((i, j))
+            perimeter = dfs(i, j + 1) # move right
+            perimeter += dfs(i, j - 1)
+            perimeter += dfs(i + 1, j)
+            perimeter += dfs(i - 1, j)
+            return perimeter
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]:
+                    return dfs(i, j)
