4246084112790b3194bd1e58887d3455fdc6109a
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-06-18 20:54:36 +0200

    Time: 400 ms (38.98%), Space: 16.9 MB (89.48%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -2,24 +2,37 @@ class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         # O(n*m) # TC
         # O(n*m) # SC
-        visit = set() # cells visited
+        # visit = set() # cells visited
 
-        def dfs(i, j):
-            if (i >= len(grid) or j >= len(grid[0]) 
-                or i < 0 or j < 0 or grid[i][j] == 0):
-                return 1
+        # def dfs(i, j):
+        #     if (i >= len(grid) or j >= len(grid[0]) 
+        #         or i < 0 or j < 0 or grid[i][j] == 0):
+        #         return 1
 
-            if (i, j) in visit:
-                return 0
+        #     if (i, j) in visit:
+        #         return 0
 
-            visit.add((i, j))
-            perimeter = dfs(i, j + 1) # move right
-            perimeter += dfs(i, j - 1) # move left
-            perimeter += dfs(i + 1, j) # move up
-            perimeter += dfs(i - 1, j) # move down
-            return perimeter
+        #     visit.add((i, j))
+        #     perimeter = dfs(i, j + 1) # move right
+        #     perimeter += dfs(i, j - 1) # move left
+        #     perimeter += dfs(i + 1, j) # move up
+        #     perimeter += dfs(i - 1, j) # move down
+        #     return perimeter
 
+        # for i in range(len(grid)):
+        #     for j in range(len(grid[0])):
+        #         if grid[i][j]:
+        #             return dfs(i, j)
+
+        perimeter = 0
+    
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j]:
-                    return dfs(i, j)
+                if grid[i][j] == 1:
+                    perimeter += 4
+                    if i > 0 and grid[i - 1][j] == 1:
+                        perimeter -= 2
+                    if j > 0 and grid[i][j - 1] == 1:
+                        perimeter -= 2
+
+        return perimeter
