182c6bf5eb1956fbfb2a78e09193456dee81a51f
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-06-22 14:33:13 +0200

    Time: 35 ms (96.76%), Space: 17.5 MB (98.09%) - LeetHub

--- 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.py
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.py
@@ -6,24 +6,35 @@
 #         self.right = right
 class Solution:
     def tree2str(self, root: Optional[TreeNode]) -> str:
-        # preorder traversal
-        answer = [] # str("") | adding strings together is not efficient
-
         def preorder(node):
-            if not node:
-                return 
-            
-            answer.append("(")
-            answer.append(str(node.val))
-
-            if not node.left and node.right:
-                answer.append("()")
-
-            preorder(node.left)
-            preorder(node.right)
-
-            answer.append(")")
-            
-            return "".join(answer[1:-1])
+            if not node.left and not node.right:
+                return str(node.val)
+            elif node.left and not node.right:
+                return f"{node.val}({preorder(node.left)})"
+            elif not node.left and node.right:
+                return f"{node.val}()({preorder(node.right)})"
+            else:
+                return f"{node.val}({preorder(node.left)})({preorder(node.right)})"
 
         return preorder(root)
+        # preorder traversal
+        # answer = [] # str("") | adding strings together is not efficient
+
+        # def preorder(node):
+        #     if not node:
+        #         return 
+            
+        #     answer.append("(")
+        #     answer.append(str(node.val))
+
+        #     if not node.left and node.right:
+        #         answer.append("()")
+
+        #     preorder(node.left)
+        #     preorder(node.right)
+
+        #     answer.append(")")
+            
+        #     return "".join(answer[1:-1])
+
+        # return preorder(root)
