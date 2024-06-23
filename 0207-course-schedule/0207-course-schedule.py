78bd9368be91bb37f4a4971cd743a30944dfe4fc
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-06-23 19:07:05 +0200

    Time: 90 ms (55.14%), Space: 18.3 MB (70.07%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,27 +1,35 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        pre = defaultdict(list)
-        for course, p in prerequisites:
-            pre[course].append(p)
+        # no cycles
+        # directed graph without both directions
+
+        graph = collections.defaultdict(list)
+
+        # building a directed graph 
+        for course, prerequisite in prerequisites:
+            graph[course].append(prerequisite)
         
-        taken = set()
+        visited = set()
 
         def dfs(course):
-            if not pre[course]:
-                return True
-            
-            if course in taken:
+            # detected a cycle/loop 
+            if course in visited:
                 return False
-            
-            taken.add(course)
 
-            for p in pre[course]:
-                if not dfs(p): 
+            if graph[course] == []:
+                return True
+
+            visited.add(course)
+            for pre in graph[course]:
+                if not dfs(pre):
                     return False
             
-            pre[course] = []
+            # remove from the visited set because we finish visit it
+            visited.remove(course)
+            # setting course to [] because it works
+            graph[course] = []
             return True
-        
+
         for course in range(numCourses):
             if not dfs(course):
                 return False
