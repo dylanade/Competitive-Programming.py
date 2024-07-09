5f7f5f8040c7423b945bb691319e743e5f9679f8
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-07-09 22:06:53 +0200

    Time: 462 ms (64.3%), Space: 29.5 MB (78.22%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,5 +1,29 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        ## Sorting operation: O(n*log(n))
         nums.sort(reverse=True)
         return nums[k-1]
-        
+
+        # Using max heap: O(n+k*log(n))
+
+        # # Quick Select: Avg Case Time Complexity: O(n) with worst case: O(n*2) 
+        # k = len(nums) - k
+
+        # def quickSelect(l, r):
+        #     pivot, p = nums[r], l
+
+        #     for i in range(l, r):
+        #         if nums[i] <= pivot:
+        #             nums[p], nums[i] = nums[i], nums[p]
+        #             p += 1
+        #         nums[p], nums[r] = nums[r], nums[p]
+
+        #         if p>k: 
+        #             return quickSelect(l, p-1)
+        #         elif p<k: 
+        #             return quickSelect(p + 1, r)
+        #         else: #p == k
+        #             return nums[p]
+
+        # return quickSelect(0, len(nums) - 1)
+
