d81c840583a6537bace9aac717cdeb75c6f133f7
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-07-09 19:10:12 +0200

    Time: 813 ms (24.02%), Space: 27.3 MB (40.16%) - LeetHub

---
+++ 2016-reduction-operations-to-make-the-array-elements-equal/2016-reduction-operations-to-make-the-array-elements-equal.py
@@ -0,0 +1,24 @@
+class Solution:
+    def reductionOperations(self, nums: List[int]) -> int:
+        if len(set(nums)) == 1:
+            return 0
+        else:
+            count = {}
+            nums.sort(reverse=True)
+            
+            #freq dictionary keep track of freq's
+            for num in nums:
+                count[num] = count.get(num, 0) + 1
+            
+            #total number of operations required
+            operations = 0
+            changes = 0
+            
+            for num in sorted(count, reverse=True):
+                count[num] += changes
+                operations += count[num]
+                changes = count[num]
+            
+            return operations-changes
+                
+            
