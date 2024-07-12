dfaa90e28201c02f2aedc822e51c9df6e04c14ab
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-07-12 21:36:47 +0200

    Time: 87 ms (5.38%), Space: 16.5 MB (84.08%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -0,0 +1,20 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        freq = collections.defaultdict(list)
+
+        for word in words:
+            count = words.count(word)
+            if word not in freq[count]:
+                freq[count].append(word)
+
+        answer = []
+        for frequency in sorted(freq, reverse=True):
+            for word in sorted(freq[frequency]):
+                if k:
+                    answer.append(word)
+                    k -= 1
+
+        return answer
+
+
+        
