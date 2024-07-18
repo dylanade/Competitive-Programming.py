3ea3387ded4a509e6f21926fdfec7807525ae451
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-07-18 21:17:45 +0200

    Time: 41 ms (73.26%), Space: 16.7 MB (18.16%) - LeetHub

---
+++ 1095-two-city-scheduling/1095-two-city-scheduling.py
@@ -0,0 +1,26 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        
+        differences = []
+
+        for cost in costs:
+            a, b = cost
+            differences.append((a, b, a-b))
+
+        differences = sorted(differences, key=lambda city: city[2])
+        n = len(costs)
+        total = 0
+
+        for i in range(n // 2):
+            total += differences[i][0]
+
+        for i in range(n // 2, n):
+            total += differences[i][1]
+
+        return total
+        
+
+
+
+
+
