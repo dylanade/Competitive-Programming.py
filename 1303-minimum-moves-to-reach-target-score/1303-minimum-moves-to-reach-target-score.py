1e964dfe7e5f3df05ca7952cf6444754f4b06a49
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-08-03 15:59:05 +0200

    Time: 43 ms (8.46%), Space: 16.5 MB (81.59%) - LeetHub

--- 1303-minimum-moves-to-reach-target-score/1303-minimum-moves-to-reach-target-score.py
+++ 1303-minimum-moves-to-reach-target-score/1303-minimum-moves-to-reach-target-score.py
@@ -1,6 +1,7 @@
 class Solution:
     def minMoves(self, target: int, maxDoubles: int) -> int:
         count_moves = 0
+        
         while maxDoubles and target > 1:
             if target % 2 == 0 and maxDoubles:
                 count_moves += 1
@@ -9,7 +10,7 @@ class Solution:
             else:
                 target -= 1
                 count_moves += 1
-                
+
         if target > 1:
             count_moves += target - 1
 
