d4c00d1195fe0be7538c766e99dd961b3b4b6a2b
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-08-29 19:44:25 +0200

    Time: 32 ms (73.77%), Space: 16.4 MB (89.02%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -1,12 +1,13 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        binary = f'{num:b}'
-        complement = ''
-        
-        for bit in binary:
-            if bit == '1':
-                complement += '0'
-            else:
-                complement += '1'
-                
-        return int(complement, 2)
+        # def flip(b):
+        #     a = ''
+        #     for i in range(len(b)):
+        #         a += '1' if b[i] == '0' else '0'
+        #     return a
+
+        # return int(flip(bin(num)[2:]), 2)
+
+        bit_length = num.bit_length()
+        mask = (1 << bit_length) - 1
+        return num ^ mask
