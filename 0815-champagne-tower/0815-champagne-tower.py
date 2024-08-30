ffc51caaf9a411ec42855c15a785656607574b73
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-08-30 21:28:56 +0200

    Time: 68 ms (69.38%), Space: 16.4 MB (100%) - LeetHub

--- 0815-champagne-tower/0815-champagne-tower.py
+++ 0815-champagne-tower/0815-champagne-tower.py
@@ -6,7 +6,6 @@ class Solution:
 
         # TREE 
         # store each level as an array
-
         # take half of parent for each child
 
         prev_row = [poured] # 1, 2, 3 ... , n
@@ -16,11 +15,11 @@ class Solution:
 
             for i in range(row):
                 excess = prev_row[i] - 1
-                poured_over = 0.5 * excess
+                overflow = 0.5 * excess
 
                 if excess > 0:
-                    cur_row[i] += poured_over
-                    cur_row[i + 1] += poured_over
+                    cur_row[i] += overflow
+                    cur_row[i + 1] += overflow
 
             prev_row = cur_row
 
