74082fa60866af38552112b8e7c1def3935e5ce3
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-09-09 22:16:27 +0200

    Time: 161 ms (91.6%), Space: 24.6 MB (5.9%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,33 +1,26 @@
+import collections
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         n = len(nums)
-        mp = {}
-        check_twoSum = set() #check whether the indices were saved
+        check = set() #check whether the indices were saved
+        save = collections.defaultdict(list)
         answer = []
 
         for a in range(n):
             for b in range(a+1, n):
-                twoSum = nums[a] + nums[b]
-                twoSum_tuple = (nums[a], nums[b])
-                
-                if twoSum_tuple in check_twoSum:
-                    continue
-                else:
-                    check_twoSum.add(twoSum_tuple)
-                    if twoSum in mp:
-                        mp[twoSum].append((a, b))
-                    else:
-                        mp[twoSum] = [(a, b)]
-
-        #print(num_to_idx)
+                two_sum = nums[a] + nums[b]
+                sum_tuple = (nums[a], nums[b])
+                if sum_tuple not in check:
+                    check.add(sum_tuple)
+                    if (a, b) not in save[two_sum]:
+                        save[two_sum].append((a, b))
 
         for a in range(n):
             for b in range(a+1, n):
                 diff = target - nums[a] - nums[b]
-                if diff in mp:
-                    for c, d in mp[diff]:
-                        if a != c and a != d and b!=c and b!=d:
-                            #print([nums[a], nums[b], nums[c], nums[d]])
+                if diff in save:
+                    for c, d in save[diff]:
+                        if a!=c and a!=d and b!=c and b!=d:
                             result = sorted([nums[a], nums[b], nums[c], nums[d]])
                             if result not in answer:
                                 answer.append(result)
