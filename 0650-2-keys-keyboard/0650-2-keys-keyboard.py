73ecc4a1deed0c5360baf9ebf9bf6f360fe54495
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-10-04 21:26:41 +0200

    Time: 28 ms (95.54%), Space: 16.4 MB (86.79%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,19 +1,30 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        def dp(n, i):
+        import math
+
+        def factors(a):
+            maxFactor = 1
+            for i in range(1, int(math.sqrt(a) + 1)):
+                if a % i == 0:
+                    if a // i != a:
+                        maxFactor = max(maxFactor, a // i)
+            return maxFactor
+            
+            
+        def copy(n):
+            ans = 0
+            
             if n == 1:
-                return i
-
-            start = 2
-            while start < n:
-                if n%start == 0:
-                    i += start
+                return 0
+            
+            while True:
+                g = factors(n)
+                if g == 1:
+                    ans += n
                     break
-                start += 1
-            else:
-                i += n
+                ans += n // g
+                n = g
+            
+            return ans
 
-            n = n // start
-            return dp(n, i)
-
-        return dp(n, 0)
+        return copy(n)
