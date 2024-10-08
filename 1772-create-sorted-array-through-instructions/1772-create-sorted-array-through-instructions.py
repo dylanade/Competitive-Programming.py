89fc61c5631a50ed9a35d92bfc55e4cf6cc78247
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-10-08 21:56:35 +0200

    Time: 2952 ms (49.62%), Space: 32.6 MB (29.62%) - LeetHub

---
+++ 1772-create-sorted-array-through-instructions/1772-create-sorted-array-through-instructions.py
@@ -0,0 +1,14 @@
+class Solution:
+    def createSortedArray(self, instructions: List[int]) -> int:
+        from sortedcontainers import SortedList
+        MOD = 10**9 + 7
+        ans = SortedList()
+        count = 0
+        
+        for i in instructions:
+            less = ans.bisect_left(i)
+            more = len(ans) - ans.bisect_right(i)
+            ans.add(i)
+            count += min(less, more)
+        
+        return count % MOD
