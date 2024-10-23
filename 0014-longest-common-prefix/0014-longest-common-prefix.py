92abda8d4f4c803ab7269d841e2b862908fa0a8b
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2024-10-23 20:58:10 +0200

    Time: 0 ms (100%), Space: 16.5 MB (97.43%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,21 +1,16 @@
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
         length = 0
+        sequence = ""
+        sort = sorted(strs)
+        first = sort[0]
+        last = sort[-1]
 
-        for word in strs:
-            sequence = ""
-            sort = sorted(strs)
-            first = sort[0]
-            last = sort[-1]
-
-            for i in range(min(len(first), len(last))):
-                if (first[i] != last[i]):
-                    return sequence
-                sequence += first[i]
-            return sequence
+        for i in range(min(len(first), len(last))):
+            if (first[i] != last[i]):
+                return sequence
+            sequence += first[i]
             
+        return sequence
+        
         
