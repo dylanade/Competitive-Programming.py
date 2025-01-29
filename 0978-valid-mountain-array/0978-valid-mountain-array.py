b3d7549fb53faa27c0a84cfe94f709952e856e9f
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2025-01-29 15:00:47 +0200

    Time: 175 ms (9.93%), Space: 19.2 MB (20.89%) - LeetHub

--- 0978-valid-mountain-array/0978-valid-mountain-array.py
+++ 0978-valid-mountain-array/0978-valid-mountain-array.py
@@ -1,22 +1,25 @@
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
-        n = len(arr)
         peak = max(arr)
         peak_pos = arr.index(peak)
-        
-        i, j = peak_pos, peak_pos
 
-        if peak_pos == 0 or peak_pos == n-1:
+        if peak_pos == 0 or peak_pos == len(arr) - 1:
             return False
 
-        while i>0:
-            if not (arr[i-1]<arr[i]):
+        i, j = peak_pos-1, peak_pos+1
+
+        prev_down = peak
+        while i >= 0:
+            if arr[i] >= prev_down:
                 return False
+            prev_down = arr[i]
             i -= 1
 
-        while j<n-1:
-            if not (arr[j]>arr[j+1]):
+        prev_up = peak
+        while j < len(arr):
+            if arr[j] >= prev_up:
                 return False
-            j += 1
+            prev_up = arr[j]
+            j += 1 
 
         return True
