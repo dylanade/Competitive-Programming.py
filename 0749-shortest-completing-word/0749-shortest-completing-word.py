2c22723bdc05ccf1a1af08c5ae20ef05d31ab3fd
Dylan Govender <134143760+dylan-govender@users.noreply.github.com>
2025-02-05 21:08:17 +0200

    Time: 4 ms (87.35%), Space: 18 MB (38.28%) - LeetHub

--- 0749-shortest-completing-word/0749-shortest-completing-word.py
+++ 0749-shortest-completing-word/0749-shortest-completing-word.py
@@ -2,11 +2,11 @@ class Solution:
     def shortestCompletingWord(self, licenseChars: str, words: List[str]) -> str:
         licenseChars = [char.lower() for char in licenseChars if char.isalpha()]
         countChar = collections.Counter(licenseChars)
-        max_length = max([len(word) for word in words])
-
         answer = []
+
         for word in words:
             skip = False
+            
             for char in countChar:
                 if word.count(char) < countChar[char]:
                     skip = True
@@ -14,10 +14,6 @@ class Solution:
 
             if not skip:
                 answer.append(word)
-
         
         answer.sort(key=len)
         return answer[0]
-                
-            
-            
